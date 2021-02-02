9b07023a1071ab4022fdef1ac2de88ba6ac1a325
audricus19 <audricus2019@gmail.com>
2021-02-02 12:22:59 -0500

    Add betawaitlist dialog

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en-US">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.jQuery ||
+                document.write(
+                  '<scr' +
+                    'ipt src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"><\/sc' +
+                    'ript>',
+                );`
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.$kol_jquery = window.jQuery;`
+            }}
+          />
+          <script src="https://cdn.kickoffpages.com/widgets/1.9.9/kol_bounce.js"></script>
+          <script src="https://cdn.kickoffpages.com/w/151168/308230.js"></script>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
